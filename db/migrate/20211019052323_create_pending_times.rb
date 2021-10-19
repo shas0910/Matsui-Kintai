f586e917b2ddc82e3224fb4a821da1b5ee1bc29c
daichi-fukui <fukui.daichi0913@icloud.com>
2021-10-19 14:58:21 +0900

    pending_timeテーブルの作成（途中）

---
+++ db/migrate/20211019052323_create_pending_times.rb
@@ -0,0 +1,16 @@
+class CreatePendingTimes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :pending_times do |t|
+      # t.references :time, foreign_key: true, null: false
+      t.time :start
+      t.time :finish
+      t.time :break_start
+      t.time :break_finish
+      t.string :status
+      t.text :comment_request
+      t.text :comment_permission
+
+      t.timestamps
+    end
+  end
+end
---
+++ test/models/pending_time_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class PendingTimeTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
