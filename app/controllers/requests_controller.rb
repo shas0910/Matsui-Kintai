5065d0eb77cd07d347f7963c0a990ead6ce1fc6e
shas0910 <shas19900910@gmail.com>
2021-10-25 18:40:15 +0900

    こみっと

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,7 +1,8 @@
 class RequestsController < ApplicationController
 
   def index
-    @user = current_user
+    @pending_timecards = PendingTimecard.where(id: Timecard.where(user_id: current_user.id).ids)
+    @pending_schedules = PendingSchedule.where(id: Schedule.where(user_id: current_user.id).ids)
   end
 
 end
