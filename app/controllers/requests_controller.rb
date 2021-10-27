d9a6cba03e5dfef85f91fad0829fa0d533002e14
daichi-fukui <fukui.daichi0913@icloud.com>
2021-10-27 18:34:36 +0900

    申請管理機能

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -2,6 +2,7 @@ class RequestsController < ApplicationController
 
   def index
     @pending_timecards = PendingTimecard.where(timecard_id: Timecard.where(user_id: current_user.id).ids)
+    @pending_schedules = PendingSchedule.where(schedule_id: Schedule.where(user_id: current_user.id).ids)
   end
 
 end
