47d6c5391ecb8f552482ac6a987cc8e4ac385c3d
shas0910 <shas19900910@gmail.com>
2021-11-18 17:48:36 +0900

    JQ動かない

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,8 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+require("jquery")
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
@@ -15,5 +17,3 @@ import "../flash"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
-require("jquery")
