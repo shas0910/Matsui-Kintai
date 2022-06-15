490d5ca55d20b6d6306537ef7d885cf1f5624c6c
shas0910 <shas19900910@gmail.com>
2022-06-15 17:31:05 +0900

    毎日の有休付与日、削除処理

--- lib/batch/grant.rb
+++ lib/batch/grant.rb
@@ -1,20 +1,23 @@
-#有給付与の処理
-
+#毎日の有休管理処理
 class Batch::Grant
   def self.grant
-    User.where.not(user_type: "管理者").each do |u|
-      if Date.today.month == u.grant_date.month && Date.today.day == u.grant_date.day
-        paid_vacation =  PaidVacation.find_or_initialize_by(user_id: u.id)
-        if paid_vacation.new_record?
-          paid_vacation.grant = 0
-          paid_vacation.carry = 0
-          paid_vacation.save
-        end
-        paid_vacation.update_attribute(:carry, PaidVacation.find_by(user_id: u.id).grant)
-        paid_vacation.update_attribute(:grant, u.grant_limit)
-        puts "grant days to " + u.full_name
+    # 付与日から2年経過した有休を消す
+    PaidVacation.all.each do |pv|
+      if pv.granted_date.since(2.years) <= Date.today
+        puts "delete #{pv.remain} days from #{User.find(pv.user_id).full_name}"
+        pv.destroy
       end
     end
-    puts "all granted"
+    # 有休付与
+    User.where.not(user_type: "管理者").each do |u|
+      if Date.today.month == u.grant_date.month && Date.today.day == u.grant_date.day
+        paid_vacation =  PaidVacation.new
+        paid_vacation.user_id = u.id
+        paid_vacation.remain = u.grant_limit
+        paid_vacation.granted_date = Date.today
+        paid_vacation.save
+        puts "grant #{paid_vacation.remain} days to #{u.full_name}"
+      end
+    end
   end
 end
